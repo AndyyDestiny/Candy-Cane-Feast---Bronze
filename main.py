9e1f795d87a0a45e153bf96d72bb66f0ad935cb2
AndyyDestiny <134987155+AndyyDestiny@users.noreply.github.com>
2024-01-08 22:47:30 -0500

    main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ candy = [eval(i) for i in input().split(" ")]
 for i in candy:
     height = 0
     for z in range(info[0]):
-        if cows[z] >= i:
-            cows[z] += i - height
-            break
         if cows[z] >= height:
+            if cows[z] >= i:
+                cows[z] += i - height
+                break
             gain = cows[z] - height
             cows[z] += gain
             height += gain
